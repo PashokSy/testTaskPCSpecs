23a4de764f00292f6781606dd5789e6a24b6328e
Pavlo Syzonenko <pavlo.syzonenko@gmail.com>
2019-10-03 23:49:08 +0300

    Add project files.

---
+++ testTaskPCSpecs/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testTaskPCSpecs {
+    class Program {
+        static void Main() {
+            PCSpecs PC = new PCSpecs();
+
+            Console.WriteLine( "PC Name: " + PC.Name );
+            Console.WriteLine( "Manufacturer: " + PC.Manufacturer );
+            Console.WriteLine( "ListOfUsers:" );
+            foreach( var item in PC.ListOfUsers ) {
+                Console.WriteLine( " -" + item );
+            }
+
+            var performanceCounterCategories = PerformanceCounterCategory.GetCategories()
+
+            .FirstOrDefault( category => category.CategoryName == "Memory" );
+
+            var performanceCounters = performanceCounterCategories.GetCounters();
+
+            Console.WriteLine( "Displaying performance counters for Memory category:--\n" );
+
+            foreach( PerformanceCounter performanceCounter in performanceCounters ) {
+
+                Console.WriteLine( performanceCounter.CounterName );
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
